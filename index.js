f959e5b98dd37ff189c4d9d459383cb045743e74
unknown <robakidzenino89@gmail.com>
2023-06-15 00:04:17 +0400

    jsori

---
+++ index.js
@@ -0,0 +1,15 @@
+let x = 10;
+let y = 40;
+let z = 6;
+
+switch(true){
+    case ( x >= y && x >= z):
+        console.log("x")
+         break;
+    case (y >= x && y >= z):
+         console.log("y")
+         break;
+    case (z >= x && z >= z):
+        console.log("z")
+         break;
+}
